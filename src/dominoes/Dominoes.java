25a12c10fb2e7a69e8843a32c2e05f35fe399deb
Usuario 147 Sala 2 Facultad de Ciencias <usuario147_med@medellin.loc>
2019-07-29 16:07:34 -0500

    arreglado ficha aleatoria

--- src/dominoes/Dominoes.java
+++ src/dominoes/Dominoes.java
@@ -20,7 +20,6 @@ public class Dominoes {
         // TODO code application logic here
         Vector<Integer> dealer = new Vector<Integer>();
         Vector<String> fichas = new Vector<String>();
-        Vector<String> jugador1 = new Vector<String>();
         fichas.add("0|0");
         fichas.add("0|1");
         fichas.add("0|2");
@@ -57,35 +56,89 @@ public class Dominoes {
             System.out.println("Escoja entre 2 a 5 jugadores");
             numJugadores = input.nextInt();
         }
-        
-        if(numJugadores == 2 ||  numJugadores == 3){
-            numFichasPorJugador = 7;
-        } else if (numJugadores == 4 || numJugadores ==5){
-            numFichasPorJugador = 5;
-        }
-        
+        crearJugadores(fichas, numJugadores);
+        //System.out.println(fichas);
         
     }
     
     
-    public static void getFicha(Vector<String> fichas,Vector<String> jugador){
+    public static void getFicha(Vector<String> fichas, Vector<String> jugador){
         
-        double rand  = Math.random() * ((fichas.size() + 1));
-        int ificha = (int)rand;
+        double rand  = Math.random() * ((fichas.size()+1));
+        int ificha = (int)rand -1;
+        if(ificha < 0){
+            ificha = (int)rand;
+        }
         String ficha;
-        ficha = fichas.get(ificha);
+        ficha  = fichas.get(ificha);
         jugador.add(ficha);
         fichas.remove(ificha);
         
         
     }
     
-    public static void crearJugadores(int numJugadores){
-        int numFichasPorJugador;
-     if(numJugadores == 2 ||  numJugadores == 3){
+    public static void crearJugadores(Vector<String> fichas,int numJugadores){
+        int numFichasPorJugador=0;
+        if(numJugadores == 2 ||  numJugadores == 3){
             numFichasPorJugador = 7;
         } else if (numJugadores == 4 || numJugadores ==5){
             numFichasPorJugador = 5;
         }
+        Vector<String> jugador1= new  Vector<>();
+        Vector<String> jugador2= new  Vector<>();
+        Vector<String> jugador3= new  Vector<>();
+        Vector<String> jugador4= new  Vector<>();
+        Vector<String> jugador5= new  Vector<>();
+        
+        switch (numJugadores){
+            case 2:
+                for(int i =0; i<  numFichasPorJugador; i++){
+                    getFicha(fichas, jugador1);
+                    getFicha(fichas, jugador2);
+                }
+                System.out.print(jugador1);
+                System.out.print(jugador2);
+              
+                break;
+            case 3:
+                for(int i =0; i<  numFichasPorJugador; i++){
+                    getFicha(fichas, jugador1);
+                    getFicha(fichas, jugador2);
+                    getFicha(fichas, jugador3);
+                }
+                System.out.print(jugador1);
+                System.out.print(jugador2);
+                System.out.print(jugador3);
+               
+                break;
+            case 4:
+                for(int i =0; i<  numFichasPorJugador; i++){
+                    getFicha(fichas, jugador1);
+                    getFicha(fichas, jugador2);
+                    getFicha(fichas, jugador3);
+                    getFicha(fichas, jugador4);
+                }
+                System.out.print(jugador1);
+                System.out.print(jugador2);
+                System.out.print(jugador3);
+                System.out.print(jugador4);
+                break;
+                
+            case 5:
+                for(int i =0; i<  numFichasPorJugador; i++){
+                    getFicha(fichas, jugador1);
+                    getFicha(fichas, jugador2);
+                    getFicha(fichas, jugador3);
+                    getFicha(fichas, jugador4);
+                    getFicha(fichas, jugador5);
+                }
+                System.out.print(jugador1);
+                System.out.print(jugador2);
+                System.out.print(jugador3);
+                System.out.print(jugador4);
+                System.out.print(jugador5);
+                break;
+        }
+        
     }
 }
